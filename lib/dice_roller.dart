b170043b81121cc1fce61b1fa98f935641f3210b
Furkan Şen <furkansen202@gmail.com>
2024-04-26 01:51:34 +0300

    I added another dice using the row and expanded widgets. When the application was started, the two dice were dynamically adjusted to receive random values, and two different variables were used to prevent the same values from appearing when the button was pressed.

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 final randomizer = Random();
+final randomizer2 = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -13,11 +14,13 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var currentDiceRoll = 2;
+  var currentDiceRoll = randomizer.nextInt(6) + 1;
+  var currentDiceRoll2 = randomizer2.nextInt(6) + 1;
 
   void rollDice() {
     setState(() {
       currentDiceRoll = randomizer.nextInt(6) + 1;
+      currentDiceRoll2 = randomizer2.nextInt(6) + 1;
     });
   }
 
@@ -26,9 +29,22 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(
-          'assets/images/dice-$currentDiceRoll.png',
-          width: 200,
+        Row(children: [
+          Expanded(
+            child: Image.asset(
+              'assets/images/dice-$currentDiceRoll.png',
+              width: 200,
+            ),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/images/dice-$currentDiceRoll2.png',
+              width: 200,
+            ),
+          ),
+        ]),
+        const SizedBox(
+          height: 40,
         ),
         TextButton(
           onPressed: rollDice,
